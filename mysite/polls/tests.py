f5931991a0d8f92855b6a1805a0bb80a5d6a8788
Shreesha Kedlaya <Shreesha.Kedlaya@viacom18.com>
2019-02-25 23:54:07 +0530

    Django starter

---
+++ mysite/polls/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
